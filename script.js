f9cfa99ec45f9273171d3464157df7aed5903296
Chance Goodwin <cgood1413@gmail.com>
2021-12-03 02:35:37 -0600

    Began assignment and troubleshooting

--- script.js
+++ script.js
@@ -1,16 +1,75 @@
-// Write your JavaScript code here!
+const init = () => {
 
-window.addEventListener("load", function() {
+    const form = document.querySelector("form")
+    const faultyList = document.querySelector("div[id=faultyItems]");
+    const pilotInput = document.querySelector("input[name=pilotName]");
+    const copilotInput = document.querySelector("input[name=copilotName]");
+    const fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+    const cargoLevelInput = document.querySelector("input[name=cargoMass]");
+    
+    
+    //This is where I'm running into issues getting my files to cooperate. So far if I include everything under init() it works fine (more or less) but otherwise it's not much use
+    form.addEventListener("submit", (event) => {
+        
+        event.preventDefault();
+        formSubmission(document, faultyList, pilotInput, copilotInput, fuelLevelInput, cargoLevelInput);
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
-});
+    });
+
+    // const formSubmission = (document, list, pilot, copilot, fuelLevel, cargoLevel) => {
+
+    //     const valueObjs = [
+    //       pilotObj = {
+    //         value: pilot.value.trim(),
+    //         defaultType: "Not a Number",
+    //       },
+    //       copilotObj = {
+    //         value: copilot.value.trim(),
+    //         defaultType: "Not a Number",
+    //       },
+    //       fuelObj = {
+    //         value: fuelLevel.value.trim(),
+    //         defaultType: "Is a Number",
+    //       },
+    //       cargoObj = {
+    //         value: cargoLevel.value.trim(),
+    //         defaultType: "Is a Number",
+    //       },
+    //     ];
+
+    //     const validatedValues = validateInput(valueObjs);
+    //     if (validatedValues.includes("Empty")){
+    //         alert("All fields are required");
+    //     };
+        
+    // };
+
+    // const validateInput = (objArr) => {
+
+    //     const mappedValues = objArr.map((obj) => {
+    //       if (obj.value === "") {
+    //         return "Empty";
+    //       } else if (isNaN(obj.value)) {
+    //         return "Not a Number";
+    //       } else {
+    //         return "Is a Number";
+    //       }
+    //     });
+    //     return mappedValues;
+
+    // };
+    
+    // let listedPlanets;
+    // // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    // let listedPlanetsResponse;
+    // listedPlanetsResponse.then(function (result) {
+    //     listedPlanets = result;
+    //     console.log(listedPlanets);
+    // }).then(function () {
+    //     console.log(listedPlanets);
+    //     // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+    // })
+
+}
+
+window.addEventListener("load", init);
--- scriptHelper.js
+++ scriptHelper.js
@@ -16,13 +16,52 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
    */
 }
 
-function validateInput(testInput) {
-   
-}
 
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
-}
+//I'm struggling with fulfilling the assignment guidelines while still getting the expected outcome. Honestly there are other ways I'd do this if given the chance to do it from the bottom-up, but I know that's not the point of this assignment.   
+const validateInput = (objArr) => {
+
+    const mappedValues = objArr.map((obj) => {
+      if (obj.value === "") {
+        return "Empty";
+      } else if (isNaN(obj.value)) {
+        return "Not a Number";
+      } else {
+        return "Is a Number";
+      }
+    });
+    return mappedValues;
+
+};
+
+
+// I'm not sure if this array of objs is what I want to keep for the entire assignment. But at first impressions it seems useful since I want to do specific things and retrieve specific information about each type of input, but ¯\_(ツ)_/¯
+const formSubmission = (document, list, pilot, copilot, fuelLevel, cargoLevel) => {
+
+    const valueObjs = [
+      pilotObj = {
+        value: pilot.value.trim(),
+        defaultType: "Not a Number",
+      },
+      copilotObj = {
+        value: copilot.value.trim(),
+        defaultType: "Not a Number",
+      },
+      fuelObj = {
+        value: fuelLevel.value.trim(),
+        defaultType: "Is a Number",
+      },
+      cargoObj = {
+        value: cargoLevel.value.trim(),
+        defaultType: "Is a Number",
+      },
+    ];
+
+    const validatedValues = validateInput(valueObjs);
+    if (validatedValues.includes("Empty")){
+        alert("All fields are required");
+    };
+    
+};
 
 async function myFetch() {
     let planetsReturned;
