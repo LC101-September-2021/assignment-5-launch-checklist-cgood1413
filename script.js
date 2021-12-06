8d25a71dd77c3985050d61be28d8be0aa3205d85
Chance Goodwin <cgood1413@gmail.com>
2021-12-05 19:57:41 -0600

    Assignment Initial Completion

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+
 const init = () => {
 
+    const document = window.document;
     const form = document.querySelector("form")
     const faultyList = document.querySelector("div[id=faultyItems]");
     const pilotInput = document.querySelector("input[name=pilotName]");
@@ -7,59 +9,40 @@ const init = () => {
     const fuelLevelInput = document.querySelector("input[name=fuelLevel]");
     const cargoLevelInput = document.querySelector("input[name=cargoMass]");
     
-    
-    //This is where I'm running into issues getting my files to cooperate. So far if I include the functions under init() it works fine (more or less) but otherwise it's not much use
     form.addEventListener("submit", (event) => {
-        
-        event.preventDefault();
-        formSubmission(document, faultyList, pilotInput, copilotInput, fuelLevelInput, cargoLevelInput);
 
-    });
-
-    const formSubmission = (document, list, pilot, copilot, fuelLevel, cargoLevel) => {
-
-        const pilotValue = pilot.value.trim();
-        const copilotValue = copilot.value.trim();
-        const fuelValue = fuelLevel.value.trim();
-        const cargoValue = cargoLevel.value.trim();
+        const pilotValue = pilotInput.value.trim();
+        const copilotValue = copilotInput.value.trim();
+        const fuelValue = fuelLevelInput.value.trim();
+        const cargoValue = cargoLevelInput.value.trim();
         const defaultValues = ["Not a Number", "Not a Number", "Is a Number", "Is a Number"];
         const values = [pilotValue, copilotValue, fuelValue, cargoValue];
         const validatedValues = values.map(validateInput);
-
         const isCorrect = validatedValues.every((element, index) => {
             return element === defaultValues[index];
         });
 
+        event.preventDefault();
         if (validatedValues.includes("Empty")) {
             alert("All fields are required.");
+            event.preventDefault();
         } else if (!isCorrect) {
             alert("Invalid input, please review responses and resubmit.");
-        }
-    
-    };
-
-    const validateInput = (str) => {
-
-        if (str === ""){
-            return "Empty";
-        } else if (isNaN(str)){
-            return "Not a Number";
+            event.preventDefault();
         } else {
-            return "Is a Number";
+            formSubmission(document, faultyList, pilotValue, copilotValue, fuelValue, cargoValue);
         }
-
-    };
+        
+    });
     
-    // let listedPlanets;
-    // // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    // let listedPlanetsResponse;
-    // listedPlanetsResponse.then(function (result) {
-    //     listedPlanets = result;
-    //     console.log(listedPlanets);
-    // }).then(function () {
-    //     console.log(listedPlanets);
-    //     // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-    // })
+    let listedPlanets;
+    let listedPlanetsResponse = myFetch();
+    listedPlanetsResponse.then(function (result) {
+        listedPlanets = result;
+    }).then(function () {
+        const {name, diameter, star, distance, moons, image} = pickPlanet(listedPlanets);
+        addDestinationInfo(document, name, diameter, star, distance, moons, image);
+    })
 
 }
 
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,24 +1,7 @@
-// Write your helper functions here!
+
 require('isomorphic-fetch');
 
-function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
-}
-
-
-//I'm struggling with fulfilling the assignment guidelines while still getting the expected outcome. Honestly there are other ways I'd do this if given the chance to do it from the bottom-up, but I know that's not the point of this assignment.   
-const validateInput = (str) => {
+function validateInput(str) {
 
     if (str === ""){
         return "Empty";
@@ -30,40 +13,65 @@ const validateInput = (str) => {
 
 };
 
+function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 
+    const launchStatus = document.getElementById("launchStatus");
+    const pilotStatus = document.getElementById("pilotStatus")
+    const copilotStatus = document.getElementById("copilotStatus");
+    const fuelStatus = document.getElementById("fuelStatus");
+    const cargoStatus = document.getElementById("cargoStatus");
 
-const formSubmission = (document, list, pilot, copilot, fuelLevel, cargoLevel) => {
-
-    const pilotValue = pilot.value.trim();
-    const copilotValue = copilot.value.trim();
-    const fuelValue = fuelLevel.value.trim();
-    const cargoValue = cargoLevel.value.trim();
-    const defaultValues = ["Not a Number", "Not a Number", "Is a Number", "Is a Number"];
-    const values = [pilotValue, copilotValue, fuelValue, cargoValue];
-    const validatedValues = values.map(validateInput);
-
-    // const isCorrect = validatedValues.every((element, index) => {
-    //     element === defaultValues[index];
-    // })
-    
-    if (validatedValues.includes("Empty")) {
-      alert("All fields are required.");
-    } else { // After I make sure that the inputs aren't empty, obvi the next step is to ensure they are the correct type. So far I'm trying to compare two arrays, and every seems like the most effective solution since I ultimately want one boolean value. However I just can't figure out the syntax for the .every expression^ to make it happen. I could write a loop, but that would require even more steps to ensure I only get one alert for the entire form (rather than for each false match) :/
-        console.log(validatedValues[0] === defaultValues[0]);
+    list.style.visibility = "visible";
+    pilotStatus.innerText = `${pilot} is ready for launch.`;
+    copilotStatus.innerText = `${copilot} is ready for launch.`;
+    if (fuelLevel < 10000) {
+        launchStatus.style.color = "rgb(199, 37, 78)";
+        launchStatus.innerText = "Shuttle Not Ready for Launch";
+        fuelStatus.innerText = `Fuel level too low for launch`;
+    }
+    if (cargoLevel > 10000) {
+        launchStatus.style.color = "rgb(199, 37, 78)";
+        launchStatus.innerText = "Shuttle Not Ready for Launch";
+        cargoStatus.innerText = `Cargo mass too heavy for launch`;
+    }
+    if (fuelLevel >= 10000 && cargoLevel <= 10000){
+        launchStatus.style.color = "rgb(65, 159, 106)";
+        launchStatus.innerText = "Shuttle is Ready for Launch";
     }
 
 };
 
-async function myFetch() {
-    let planetsReturned;
+function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
+    
+    const missionTarget = document.getElementById("missionTarget");
+    missionTarget.innerHTML = `
+        <h2>Mission Destination</h2>
+        <ol>
+            <li>Name: ${name}</li>
+            <li>Diameter: ${diameter}</li>
+            <li>Star: ${star}</li>
+            <li>Distance from Earth: ${distance}</li>
+            <li>Number of Moons: ${moons}</li>
+        </ol>
+        <img src="${imageUrl}">
+    `;
 
-    planetsReturned = await fetch().then( function(response) {
+}
+
+async function myFetch() {
+
+    const planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+        return response.json(); 
         });
 
     return planetsReturned;
+
 }
 
 function pickPlanet(planets) {
+
+    return planets[Math.floor(Math.random()*planets.length)];
+    
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
